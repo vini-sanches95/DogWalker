c1d86d221fa1a0e0e5d44ccd6eea277bed0f73bb
Vinicius Moro Sanches <suiciniv.ms@hotmail.com>
2017-11-07 22:32:29 -0200

    primeiras activities
    activitiy #1 - transição para outras telas
    activity #2 - Maps
    activity #3 - Login

--- app/src/main/java/mds/dogwalker/InitialActivity.java
+++ app/src/main/java/mds/dogwalker/InitialActivity.java
@@ -1,7 +1,9 @@
 package mds.dogwalker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class InitialActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class InitialActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial);
+    }
+
+    public void GoToMapsActivity(View view){
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+
+    public void GoToLoginActivity(View view){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
