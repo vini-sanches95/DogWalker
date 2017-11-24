2f5c4610d7abaa33b2c4ddabb8dab85b2262b9ae
Vinicius Moro Sanches <suiciniv.ms@hotmail.com>
2017-11-23 23:00:17 -0200

    add classes usadas
    add calsses:
    Profile, Pet, Caminho, Passeio

---
+++ app/src/main/java/mds/dogwalker/Profile.java
@@ -0,0 +1,89 @@
+package mds.dogwalker;
+
+import android.provider.ContactsContract;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by ViniciusSanches on 23/11/17.
+ */
+
+enum tipoGenero {Masculino, Feminino, Outro}
+
+public class Profile {
+
+    int id;
+    String nome;
+    String sobrenome;
+    String senha;
+    Date nascimento;
+    ContactsContract.CommonDataKinds.Email email;
+    tipoGenero genero;
+    List<Pet> pets;
+    //String local;
+
+    Profile(){}
+
+    Profile(int i, String n, String s, String p, Date nasc, ContactsContract.CommonDataKinds.Email e, tipoGenero g){
+        id = i;
+        nome = n;
+        sobrenome = s;
+        email = e;
+        genero = g;
+        senha = p;
+        nascimento = nasc;
+    }
+
+    public List<Pet> GetPets(){
+        return pets;
+    }
+
+    public void SetSenha(String s){
+        senha = s;
+    }
+
+    public String GetSenha(){
+        return senha;
+    }
+
+    public void SetDataNascimento(Date n){
+        nascimento = n;
+    }
+
+    public Date GetDataNascimento(){
+        return nascimento;
+    }
+
+    public void SetNome(String n){
+        nome = n;
+    }
+
+    public void SetSobrenome(String s){
+        sobrenome = s;
+    }
+
+    public void SetEmail(ContactsContract.CommonDataKinds.Email e){
+        email = e;
+    }
+
+    public void SetGenero(tipoGenero g){
+        genero = g;
+    }
+
+    public String GetNome(){
+        return nome;
+    }
+
+    public String GetSobrenome(){
+        return sobrenome;
+    }
+
+    public ContactsContract.CommonDataKinds.Email GetEmail(){
+        return email;
+    }
+
+    public tipoGenero GetGenero(){
+        return genero;
+    }
+}
